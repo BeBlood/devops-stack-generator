e962c58474585ad70c01f85ad15eb52ac1b0fd23
Benjamin BOUDIER <benjamin.boudier@idci-consulting.fr>
2018-05-09 18:52:21 +0200

    Update: project builder script

--- project.sh
+++ project.sh
@@ -11,30 +11,42 @@ declare -A PROJECT
 # Read user input text
 function read()
 {
-SUBTITLE=$1
-INPUT=$(dialog --title "$TITLE" \
+  SUBTITLE=$1
+  INPUT=$(dialog --title "$TITLE" \
           --clear \
           --inputbox "$SUBTITLE" \
           $HEIGHT $WIDTH \
           2>&1 >/dev/tty)
 
-echo $INPUT
-} # END read
+  echo $INPUT
+} # read
+
+# Ask user for an action
+function ask()
+{
+  SUBTITLE=$1
+  $(dialog --title "$TITLE" \
+            --stdout \
+            --yesno "$SUBTITLE" \
+            $HEIGHT $WIDTH \
+            2>&1 >/dev/null)
+  echo $?
+} # ask
 
 # Read single user choice
 function get_choice()
 {
-	OPTIONS=$1
+  OPTIONS=$1
   SUBTITLE=$2
   NUMBER=$((${#OPTIONS[*]} / 2))
 
-	CHOICE=$(dialog --title "$TITLE" \
+  CHOICE=$(dialog --title "$TITLE" \
 		        --menu "$SUBTITLE" \
 		        $HEIGHT $WIDTH $NUMBER \
 		        "${OPTIONS[@]}" \
 		        2>&1 >/dev/tty)
-	echo $CHOICE
-} # END get_choice
+  echo $CHOICE
+} # get_choice
 
 # Read multiple user choices
 function get_choices()
@@ -50,7 +62,11 @@ function get_choices()
       "${OPTIONS[@]}" \
       2>&1 >/dev/tty)
   echo $CHOICES
-} # END get_choices
+} # get_choices
+
+########################
+#  FILE MANIPULATIONS  #
+########################
 
 # Update variables in .env.dist file
 function update_env()
@@ -58,48 +74,112 @@ function update_env()
   VAR_NAME=$1
 
   sed -i "s/^$VAR_NAME=.*/$VAR_NAME=${PROJECT[$VAR_NAME]}/" '.env.dist'
-} # END update_env
+} # update_env
 
+# Add a volume in docker-compose file
 function add_volume_docker_compose()
 {
   VOLUME_NAME=$1
 
   sed -i "/^volumes:/a\\  $VOLUME_NAME:" 'docker-compose.yml'
-}
+} # add_volume_docker_compose
 
+# Add container with his configuration in project from .templates dir
 function add_template()
 {
   TEMPLATE_NAME=$1
 
   # docker-compose.yml template
-  DOCKER_COMPOSE_CONFIGURATION=$(cat "$TEMPLATES_PATH/$TEMPLATE_NAME/docker-compose.yml")
-  APPEND_BEFORE_LINE=$(grep -n "^volumes:" 'docker-compose.yml' | cut -d : -f 1)
-  OUTPUT="$(awk -v "s=$DOCKER_COMPOSE_CONFIGURATION\n" -v "l=$APPEND_BEFORE_LINE" 'NR==l{print s} 1' 'docker-compose.yml')"
-  echo "$OUTPUT" > 'docker-compose.yml'
+  DC_TEMPLATE_PATH="$TEMPLATES_PATH/$TEMPLATE_NAME/docker-compose.yml"
+  if [ -f $DC_TEMPLATE_PATH ]; then
+    DOCKER_COMPOSE_CONFIGURATION=$(cat "$DC_TEMPLATE_PATH")
+    APPEND_BEFORE_LINE=$(grep -n "^volumes:" 'docker-compose.yml' | cut -d : -f 1)
+    OUTPUT="$(awk -v "s=$DOCKER_COMPOSE_CONFIGURATION\n" -v "l=$APPEND_BEFORE_LINE" 'NR==l{print s} 1' 'docker-compose.yml')"
+    echo "$OUTPUT" > 'docker-compose.yml'
+  fi
 
-  #.env template
+  # .env template
   ENV_TEMPLATE_PATH="$TEMPLATES_PATH/$TEMPLATE_NAME/.env"
   if [ -f $ENV_TEMPLATE_PATH ]; then
     ENV_CONFIGURATION=$(cat "$ENV_TEMPLATE_PATH")
-    printf "\n$ENV_CONFIGURATION" >> '.env.dist'
+    printf "\n\n$ENV_CONFIGURATION" >> '.env.dist'
   fi
 
-  #conf template
+  # .env template
+  MAKEFILE_TEMPLATE_PATH="$TEMPLATES_PATH/$TEMPLATE_NAME/Makefile"
+  if [ -f $MAKEFILE_TEMPLATE_PATH ]; then
+    MAKEFILE_COMMANDS=$(cat "$MAKEFILE_TEMPLATE_PATH")
+    printf "\n\n$MAKEFILE_COMMANDS" >> 'Makefile'
+  fi
+
+  # conf template
   ENV_TEMPLATE_PATH="$TEMPLATES_PATH/$TEMPLATE_NAME/conf"
   if [ -d $ENV_TEMPLATE_PATH ]; then
     cp -r "$ENV_TEMPLATE_PATH" "${PROJECT["PATH"]}/.docker/$TEMPLATE_NAME/"
   fi
 }
 
+# Add a new depend for specific container
+function add_depend()
+{
+  DEPEND=$(echo $1 | tr '[:lower:]' '[:upper:]')
+
+  sed -i "s/#"$DEPEND"_DEPEND//" 'docker-compose.yml'
+}
+
+# Build all the depends in docker-compose file
+function build_depends()
+{
+  DEPENDS=($(sed -E 's/(^  (\S+):)|.*/\2/' 'docker-compose.yml'))
+
+  for DEPEND in ${DEPENDS[@]}
+  do
+    add_depend $DEPEND
+  done
+} # build_depends
+
+# Get available versions for a container
+function get_available_versions()
+{
+  CONTAINER=$1
+
+  if [ -f "$TEMPLATES_PATH/$CONTAINER/.versions" ]; then
+    VERSIONS=($(cat "$TEMPLATES_PATH/$CONTAINER/.versions" | grep . | cat -n -))
+  else
+    VERSIONS=($(ls "$TEMPLATES_PATH/$CONTAINER/conf" | grep Dockerfile- | sed "s/Dockerfile-//" | cat -n -))
+  fi
+
+  echo ${VERSIONS[@]}
+} # get_available_versions
+
+##################
+#   CONTAINERS   #
+##################
+
+# Build apache configuration
+function build_apache()
+{
+    OPTIONS=($(get_available_versions ${PROJECT["SERVER"]}))
+    PROJECT["APACHE_CONTAINER_NAME"]="${PROJECT["PROJECT_NAME"]}_apache"
+    PROJECT["APACHE_VERSION"]=` echo "${OPTIONS[($(get_choice $OPTIONS "Choisissez la version de Apache")-1)*2+1]}" | grep -o "[0-9.]\+"`
+    update_env "APACHE_CONTAINER_NAME"
+    update_env "APACHE_VERSION"
+} # build_apache
+
+function build_nginx()
+{
+    OPTIONS=($(get_available_versions ${PROJECT["SERVER"]}))
+    PROJECT["NGINX_CONTAINER_NAME"]="${PROJECT["PROJECT_NAME"]}_nginx"
+    PROJECT["NGINX_VERSION"]=` echo "${OPTIONS[($(get_choice $OPTIONS "Choisissez la version de Nginx")-1)*2+1]}" | grep -o "[0-9.]\+"`
+    update_env "NGINX_CONTAINER_NAME"
+    update_env "NGINX_VERSION"
+}
+
 # Build mysql configuration
 function build_mysql()
 {
   # MYSQL
-  OPTIONS=(
-    1 "MYSQL 8.4"
-    2 "MYSQL 8.0"
-    3 "MYSQL 5.6"
-  )
+  OPTIONS=($(get_available_versions "mysql"))
   PROJECT["MYSQL_CONTAINER_NAME"]="${PROJECT["PROJECT_NAME"]}_mysql"
   PROJECT["MYSQL_VERSION"]=` echo "${OPTIONS[($(get_choice $OPTIONS "Choisissez la version de mysql")-1)*2+1]}" | grep -o "[0-9.]\+"`
   PROJECT["MYSQL_USER"]=${PROJECT["PROJECT_NAME"]}
@@ -112,17 +192,17 @@ function build_mysql()
   update_env "MYSQL_DATABASE"
 
   to_replace=`cat docker-compose.yml | grep "MYSQL_VOLUME"`
-  sed -i "s/$to_replace/  ${PROJECT["MYSQL_CONTAINER_NAME"]}:/" 'docker-compose.yml'
+  if [ -n "$to_replace" ]; then
+    sed -i "s/$to_replace/  ${PROJECT["MYSQL_CONTAINER_NAME"]}:/" 'docker-compose.yml'
+  else
+    add_volume_docker_compose "${PROJECT["MYSQL_CONTAINER_NAME"]}"
+  fi
 } # build_mysql
 
 # Build php configuration
 function build_php()
 {
-  OPTIONS=(
-    1 "PHP 5.6"
-    2 "PHP 7.0"
-    3 "PHP 7.1"
-  )
+  OPTIONS=($(get_available_versions "php"))
   PROJECT["PHP_CONTAINER_NAME"]="${PROJECT["PROJECT_NAME"]}_php"
   PROJECT["PHP_VERSION"]=` echo "${OPTIONS[($(get_choice $OPTIONS "Choisissez la version de PHP")-1)*2+1]}" | grep -o "[0-9.]\+"`
   update_env "PHP_CONTAINER_NAME"
@@ -143,30 +223,32 @@ function build_adminer()
 # Build rabbitmq configuration
 function build_rabbitmq()
 {
-    add_template "rabbitmq"
+  add_template "rabbitmq"
 
-    PROJECT["RABBITMQ_CONTAINER_NAME"]="${PROJECT["PROJECT_NAME"]}_rabbitmq"
-    PROJECT["RABBITMQ_USER"]="${PROJECT["PROJECT_NAME"]}"
-    PROJECT["RABBITMQ_PASSWORD"]="${PROJECT["PROJECT_NAME"]}"
-    update_env "RABBITMQ_CONTAINER_NAME"
-    update_env "RABBITMQ_USER"
-    update_env "RABBITMQ_PASSWORD"
+  PROJECT["RABBITMQ_CONTAINER_NAME"]="${PROJECT["PROJECT_NAME"]}_rabbitmq"
+  PROJECT["RABBITMQ_USER"]="${PROJECT["PROJECT_NAME"]}"
+  PROJECT["RABBITMQ_PASSWORD"]="${PROJECT["PROJECT_NAME"]}"
+  PROJECT["RABBITMQ_VIRTUAL_HOST"]="rabbitmq.${PROJECT["PROJECT_NAME"]}.docker"
+  update_env "RABBITMQ_CONTAINER_NAME"
+  update_env "RABBITMQ_USER"
+  update_env "RABBITMQ_PASSWORD"
+  update_env "RABBITMQ_VIRTUAL_HOST"
 } # build_rabbitmq
 
 # Build supervisord configuration
 function build_supervisord()
 {
-    add_template "supervisord"
+  add_template "supervisord"
 
-    PROJECT["SUPERVISORD_CONTAINER_NAME"]="${PROJECT["PROJECT_NAME"]}_supervisord"
-    update_env "SUPERVISORD_CONTAINER_NAME"
-    
-    add_volume_docker_compose "${PROJECT["SUPERVISORD_CONTAINER_NAME"]}"
-    sed -i "s/\${SUPERVISORD_VOLUME_NAME}/${PROJECT["SUPERVISORD_CONTAINER_NAME"]}/" 'docker-compose.yml'
+  PROJECT["SUPERVISORD_CONTAINER_NAME"]="${PROJECT["PROJECT_NAME"]}_supervisord"
+  update_env "SUPERVISORD_CONTAINER_NAME"
+
+  add_volume_docker_compose "${PROJECT["SUPERVISORD_CONTAINER_NAME"]}"
+  sed -i "s/\${SUPERVISORD_VOLUME_NAME}/${PROJECT["SUPERVISORD_CONTAINER_NAME"]}/" 'docker-compose.yml'
 } # build_supervisord
 
 # Build apache addons configurations
-function build_apache_addons()
+function build_addons()
 {
   OPTIONS=(
     1 "Adminer" "Adminer"
@@ -174,6 +256,7 @@ function build_apache_addons()
     3 "Supervisord" "Supervisord"
   )
   CHOICES=$(get_choices $OPTIONS "Voulez vous une de ces dépendences additionelles")
+  echo ${CHOICES[@]}
   for CHOICE in $CHOICES
   do
       case $CHOICE in
@@ -189,47 +272,97 @@ function build_apache_addons()
       esac
   done
   clear
-} # END build_apache_addons
+} # build_addons
+
+###############
+#   PROJECTS  #
+###############
 
 # Build the base of a project
 function build_project()
 {
   PROJECT["STACK"]=$1
-  shopt -s dotglob
-  cp -r --copy-contents $STACK_PATH/${PROJECT["STACK"]}/* "."
+
+  if [ -d "$STACK_PATH/${PROJECT["STACK"]}" ]; then
+    shopt -s dotglob
+    cp -r --copy-contents $STACK_PATH/${PROJECT["STACK"]}/* "."
+  else
+    mkdir '.docker'
+    cp "$TEMPLATES_PATH/$TEMPLATE_NAME/.env" '.env.dist'
+    cp "$TEMPLATES_PATH/$TEMPLATE_NAME/docker-compose.yml" 'docker-compose.yml'
+    cp "$TEMPLATES_PATH/$TEMPLATE_NAME/Makefile" 'Makefile'
+    add_template "proxy"
+  fi
+
   update_env "PROJECT_NAME"
   update_env "NETWORK_NAME"
   update_env "PROJECT_DEV_HOST"
 
   FUNCTION="build_$(echo "${PROJECT["STACK"]}" | tr '[:upper:]' '[:lower:]')_project"
-  if [ -n "$(type $FUNCTION | grep "$FUNCTION ()")" ]; then
+  type $FUNCTION>/dev/null
+  if [ $? -eq 0 ]; then
       $FUNCTION
-      clear
+      #clear
   else
       printf "\033c"
       echo "La configuration pour cette stack n'est pas disponible"
   fi
-} # END build_project
+  build_addons
+  build_depends
+} # build_project
 
 # Build apache project
 function build_apache_project()
 {
-  OPTIONS=(
-    1 "APACHE 2.2"
-    2 "APACHE 2.4"
-  )
-  PROJECT["APACHE_CONTAINER_NAME"]="${PROJECT["PROJECT_NAME"]}_apache"
-  PROJECT["APACHE_VERSION"]=` echo "${OPTIONS[($(get_choice $OPTIONS "Choisissez la version de Apache")-1)*2+1]}" | grep -o "[0-9.]\+"`
-  update_env "APACHE_CONTAINER_NAME"
-  update_env "APACHE_VERSION"
-
+  PROJECT["SERVER"]="apache_php"
+  # APACHE
+  build_apache
   # PHP
   build_php
   # MYSQL
   build_mysql
-  # ADD-ONS
-  build_apache_addons
-} # END build_apache_project
+} # build_apache_project
+
+# Build apache project
+function build_nginx_project()
+{
+  PROJECT["SERVER"]="nginx_php"
+  # APACHE
+  build_nginx
+  # PHP
+  build_php
+  # MYSQL
+  build_mysql
+} # build_apache_project
+
+# Build custom project
+function build_custom_project()
+{
+    OPTIONS=(
+      1 "Apache"
+      2 "Nginx"
+    )
+    SERVER=$(echo "${OPTIONS[($(get_choice $OPTIONS "Choisissez votre serveur web")-1)*2+1]}" | tr '[:upper:]' '[:lower:]')
+
+    OPTIONS=(
+      1 "PHP"
+      2 "Ruby"
+    )
+    LANGUAGE=$(echo "${OPTIONS[($(get_choice $OPTIONS "Choisissez le language interpreté par le serveur")-1)*2+1]}" | tr '[:upper:]' '[:lower:]')
+
+    PROJECT["SERVER"]="$SERVER"_"$LANGUAGE"
+    add_template "$SERVER"_"$LANGUAGE"
+    "build_$SERVER"
+    add_template "$LANGUAGE"
+    "build_$LANGUAGE"
+
+
+    if [ $(ask "Voulez vous utiliser MySQL ?") -eq 0 ]; then
+        add_template "mysql"
+        build_mysql
+    fi
+
+} # END build_custom_project
 
 ######################
 #        MAIN        #
@@ -250,6 +383,10 @@ cd "${PROJECT["PATH"]}"
 
 AVAILABLE_STACKS=$(ls -d $STACK_PATH/*/ | nl | tr '\n' ' ')
 OPTIONS=($(echo $AVAILABLE_STACKS | sed -r "s~$STACK_PATH~~g" | tr '//' ' '))
+OPTIONS_SIZE=${#PROJECT[@]}
+
+OPTIONS[$(($OPTIONS_SIZE*2))]=$(($OPTIONS_SIZE+1))
+OPTIONS[$(($OPTIONS_SIZE*2+1))]="Custom"
 
 build_project "${OPTIONS[($(get_choice $OPTIONS "Choisissez une base de projet")-1)*2+1]}"
 
